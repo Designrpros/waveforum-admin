28e67ed635151baa034c8e780bfe60aa3c6b441e
Vegar Berentsen <VegarBerentsen@gmail.com>
2025-08-01 16:29:45 +0200

    git ignore firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,16 +6,16 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration is read from environment variables
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBB6iYnZPhLS4bQcblIoCnwEECCmcsUAxk",
-  authDomain: "waveform-d0bf6.firebaseapp.com",
-  projectId: "waveform-d0bf6",
-  storageBucket: "waveform-d0bf6.firebasestorage.app",
-  messagingSenderId: "674358758887",
-  appId: "1:674358758887:web:2d7c311c03466f34e88142",
-  measurementId: "G-ZHJXSM8LFW"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
